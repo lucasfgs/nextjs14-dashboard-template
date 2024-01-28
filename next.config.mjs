2512a12139be60d3451e52732e30eefb68a99730
Lucas Ferreira <lucsferreira.dev@gmail.com>
2024-01-28 14:54:25 -0400

    refactor: rename env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION,
-    AWS_COGNITO_POOL_ID: process.env.AWS_COGNITO_POOL_ID,
-    AWS_COGNITO_APP_CLIENT_ID: process.env.AWS_COGNITO_APP_CLIENT_ID,
+    COGNITO_POOL_ID: process.env.COGNITO_POOL_ID,
+    COGNITO_APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT_ID,
   },
 };
 
--- src/utils/aws/Amplify.ts
+++ src/utils/aws/Amplify.ts
@@ -3,8 +3,8 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: String(process.env.AWS_COGNITO_POOL_ID),
-      userPoolClientId: String(process.env.AWS_COGNITO_APP_CLIENT_ID),
+      userPoolId: String(process.env.COGNITO_POOL_ID),
+      userPoolClientId: String(process.env.COGNITO_APP_CLIENT_ID),
       loginWith: {
         email: true,
         username: false,
