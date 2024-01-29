8fa094183ca23301cc0ec839d23d496f7d39b303
Lucas Ferreira <lucsferreira.dev@gmail.com>
2024-01-29 17:19:51 -0400

    refactor: remove unused code

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -170,7 +170,7 @@ export default function Login() {
       </div>
 
       <span className="text-center text-sm text-muted-foreground">
-        Don't have an account?{" "}
+        Don&apos;t have an account?{" "}
         <Link
           href={"/auth/register"}
           className={
